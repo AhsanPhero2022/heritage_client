16954246b77a3b97433ae9ee41ac918adbd69127
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-08-30 17:33:55 +0600

    navbar and footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <div>
       <ScrollRestoration />
-      <div className="sticky top-0 z-50">{/* navbar */}</div>
+      <div className="sticky top-0 z-50">
+        <Navbar />
+      </div>
       <Outlet />
-      {/* Footer */}
+      <Footer />
     </div>
   );
 }
